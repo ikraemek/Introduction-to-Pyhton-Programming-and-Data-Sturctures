f27408927841a4a8ab8094dc30c7f45a52473e55
ikraemek <ikraemek@outlook.com>
2024-09-04 23:54:43 +0300

    Display a string 5 times

---
+++ chapter01/Exercise01_02.py
@@ -0,0 +1,22 @@
+
+
+
+
+	#Author:İkra Emek
+#Email: ikraemek@outlook.com
+#Github:/ikraemek
+
+''' (Display the same  message five times )  Write a 
+ program that displays Welcome to Python five times''' 
+
+
+
+
+#Display Welcome to Pytgon five times 
+print ("Welcome to Python")
+print ("Welcome to Python")
+print ("Welcome to Python")
+print ("Welcome to Python")
+print ("Welcome to Pyhton") 
+
+#Exercise0q
